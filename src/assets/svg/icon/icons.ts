57cb82d3d8d001bc26c7d634fa13107c867f92f1
62savage <6s2avage@gmail.com>
2023-09-09 21:25:15 +0900

    [FEAT] Create Responsive UI Structure for Navbar

---
+++ src/assets/svg/icon/icons.ts
@@ -0,0 +1,6 @@
+import { ReactComponent as BagIcon } from '../bag.svg';
+import { ReactComponent as SearchIcon } from '../search.svg';
+import { ReactComponent as MenuBarIcon } from '../menubar.svg';
+import { ReactComponent as LogoIcon } from '../logo.svg';
+
+export { BagIcon, SearchIcon, MenuBarIcon, LogoIcon };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,128 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { styled } from 'styled-components';
+import { palette } from '../styles/palette';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+  BagIcon,
+  SearchIcon,
+  MenuBarIcon,
+  LogoIcon,
+} from '../assets/svg/icon/icons';
 
-export default function Navbar() {
+const ICON_SIZE = 24;
+const LOGO_SIZE = 56;
+
+const menuItems = [
+  { text: 'SHOP >', link: '/products' },
+  { text: 'FEATURES', link: '/features' },
+  { text: 'SUPPORT >', link: '/support' },
+];
+
+const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/products">Products</Link>
-    </nav>
+    <>
+      <StNavbarWrapper>
+        <StMainContainer>
+          <StButtonBox onClick={() => navigate('/')}>
+            <LogoIcon width={LOGO_SIZE} height={LOGO_SIZE} />
+          </StButtonBox>
+          <StMenuBarContainer>
+            <StLeftMenus>
+              {menuItems.map((item, index) => (
+                <li>
+                  <Link to={item.link}>{item.text}</Link>
+                </li>
+              ))}
+            </StLeftMenus>
+            <StRightMenus>
+              <li>
+                <Link to="/products">SEARCH</Link>
+              </li>
+              <li>
+                <Link to="/products">BAG</Link>
+              </li>
+            </StRightMenus>
+          </StMenuBarContainer>
+
+          <StOnMobile>
+            <StButtonBox>
+              <SearchIcon width={ICON_SIZE} height={ICON_SIZE} />
+            </StButtonBox>
+            <StButtonBox>
+              <BagIcon width={ICON_SIZE} height={ICON_SIZE} />
+            </StButtonBox>
+            <StButtonBox>
+              <MenuBarIcon width={ICON_SIZE} height={ICON_SIZE} />
+            </StButtonBox>
+          </StOnMobile>
+        </StMainContainer>
+      </StNavbarWrapper>
+    </>
   );
-}
+};
+
+export default Navbar;
+
+const StRow = styled.div`
+  display: flex;
+`;
+
+const StNavbarWrapper = styled(StRow)`
+  flex-direction: column;
+  width: 100%;
+  box-sizing: border-box;
+`;
+
+const StMainContainer = styled(StRow)`
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem;
+  border-bottom: 0.5px solid ${palette.lightGray};
+`;
+
+const StLeftMenus = styled.ul`
+  display: flex;
+  & li {
+    padding: 8px 12px;
+  }
+  & li:hover {
+    font-weight: 700;
+  }
+  @media screen and (max-width: 1024px) {
+    display: none;
+  }
+`;
+const StRightMenus = styled.ul`
+  display: flex;
+  & li {
+    padding: 8px 12px;
+  }
+  @media screen and (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+const StButtonBox = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const StOnMobile = styled.section`
+  display: none;
+  gap: 0.1rem;
+  @media screen and (max-width: 1024px) {
+    display: flex;
+  }
+`;
+
+const StMenuBarContainer = styled.section`
+  display: flex;
+  flex: 1;
+  justify-content: space-between;
+
+  font-size: 0.75rem;
+  font-weight: 700;
+`;
