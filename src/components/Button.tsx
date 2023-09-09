9c33183f5c325457c958c27ba600090d5b0ba92a
UFFETKK <soyoungan02@naver.com>
2023-09-09 16:30:03 +0900

    [FIX] : Reset alias setting

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { styled } from "styled-components";
-import { palette } from "@styles/palette";
+import { palette } from "../styles/palette";
 
 type ButtonProps = {
   content: string;
