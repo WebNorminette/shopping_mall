1646bf9b799ca76936716584b70c79f7004c5347
62savage <6s2avage@gmail.com>
2023-09-16 23:24:32 +0900

    [FEAT] Assign menuData constant

---
+++ src/constats/index.ts
@@ -0,0 +1,35 @@
+export const menuData = [
+  {
+    title: 'SHOP',
+    subItems: [
+      'NEW ARRIVALS',
+      'TEES',
+      'SWEATS',
+      'TOPS & SHIRTS',
+      'DENIM',
+      'SHORTS',
+      'PANTS',
+      'KNITS',
+      'OUTERWEAR',
+      'HEADWEAR',
+      'EYEWEAR',
+      'ACCESSORIES',
+      'ALL',
+    ],
+  },
+  {
+    title: 'SUPPORT',
+    subItems: [
+      'CUSTOMER SUPPORT',
+      'SHIPPING & RETURNS',
+      'CAREERS',
+      'LEGAL',
+      'ACCESSIBILITY',
+      'CHAT',
+    ],
+  },
+  { title: 'ACCOUNT' },
+  { title: 'CHAPTERS' },
+  { title: 'FEATURES' },
+  { title: 'SHIPPING TO: UNITED STATES / USD $' },
+];
