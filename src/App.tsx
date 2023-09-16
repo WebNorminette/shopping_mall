3c036bb27874ebf6369cfffdf28ebc620b671747
62savage <6s2avage@gmail.com>
2023-09-16 17:09:05 +0900

    [FEAT] : Reset Router Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import NotFound from './pages/NotFound';
-import Root from './pages/Root';
-import ProductDetail from './pages/ProductDetail';
+import { Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Navbar from './components/Navbar';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { AuthContextProvider } from './context/AuthContext';
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: '/products', element: <Products /> },
-      { path: '/products/:productId', element: <ProductDetail /> },
-      { path: '/products/:productId', element: <ProductDetail /> },
-    ],
-  },
-]);
-
-export default function App() {
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={true} />
+      <AuthContextProvider>
+        <Navbar />
+        <Outlet />
+      </AuthContextProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
+
+export default App;
